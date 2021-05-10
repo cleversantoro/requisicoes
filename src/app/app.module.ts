8728d8e18c001ce334457ff54ed28f66e1bcec6e
Clever Santoro Lopes <clever.lopes@pixeon.com>
2021-05-10 16:31:26 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import	{	AngularFireModule	}	from '@angular/fire';
+import	{	AngularFirestoreModule	}	from '@angular/fire/firestore';
+import	{	AngularFireAuthModule	}	from '@angular/fire/auth';
+import	{	environment	}	from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/public/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+		AngularFirestoreModule,
+		AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBO2Pl5EbG3uX0bNXNtCnmI12fA2elJLDg",
+    authDomain: "requisicoes-app-bf3e5.firebaseapp.com",
+    projectId: "requisicoes-app-bf3e5",
+    storageBucket: "requisicoes-app-bf3e5.appspot.com",
+    messagingSenderId: "1063956005191",
+    appId: "1:1063956005191:web:2add39ef5aa70e83ab8c62",
+    measurementId: "G-K5SDSNV83S"
+  }
 };
 
 /*
